62306f87edb251c51d250e2a6fb71f152c142978
sonohen <3007676+sonohen@users.noreply.github.com>
2023-01-17 14:56:30 +0000

    Enabled access "from" codespaces

--- myapp/config/environments/development.rb
+++ myapp/config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow access from anywhere because the combination of host and port is private
+  # if you run this code in CodeSpaces
+  config.hosts.clear
 end
